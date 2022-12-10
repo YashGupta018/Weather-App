de170d165a32538a7253f0c66d980631639eea80
YashGupta018 <ygupta2001@gmail.com>
2022-12-10 14:48:20 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is the initial build, everything is working fine</h1>
+    </div>
+  );
+}
+
+export default App;
